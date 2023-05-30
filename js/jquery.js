1bfda499e412c82d8f8f9c67e9ad80ad51255890
zerosr0 <sinter3@naver.com>
2023-05-30 09:45:23 +0900

    아코디언 인터렉션 추가

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,9 @@
 //leftmenu accordion
-$(function(){
-    $('.step02').hide();
-    $('.step01').on("click", function(){
-        $(this).siblings().slideToggle();
+$(function () {
+    $('ul.step02').hide();
+    $('li.step01').on("click", function () {
+        $(this).children('ul.step02').slideToggle();
+        // $(this).parent('.step01').slideUp();
         // $(this).next().stop().slideToggle();
         // console.log("click");
     })
