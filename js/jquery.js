d61711633f69167802033d9564798f850a4da233
zerosr0 <sinter3@naver.com>
2023-06-02 15:58:00 +0900

    현황페이지 수정

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,17 @@
 //leftmenu accordion
 $(function () {
-    $('ul.step02').hide();
+    // $('ul.step02').hide();
+    $('ul.step02').eq(0).show();
     $('li.step01').on("click", function () {
-        $(this).children('ul.step02').slideToggle();
+        $(this).children('ul.step02').slideToggle(150);
+        $(this).children('a').toggleClass('open');
+        // $(!this).children('ul.step02').hide();
         // $(this).parent('.step01').slideUp();
         // $(this).next().stop().slideToggle();
-        // console.log("click");
+
+    })
+    $('li.step02').on("click", function () {
+        $(this).children('ul.step03').slideToggle();
     })
 })
 
