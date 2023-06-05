c6f1f188e4fe197cd4e36a10387c0450a41996f9
zerosr0 <sinter3@naver.com>
2023-06-05 14:06:24 +0900

    fontawesome web downloads, mobile version gnb complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+const gnbLi = document.querySelectorAll('.basicMenu > ul > li')
+const gnbMenu2 = document.querySelectorAll('.basicMenu > ul > .subMenu2')
+
 const snbBtn = document.querySelector(".logoMenu .category");
 const snbMenu = document.querySelector("#pWrapSnb");
 const iframe = document.querySelector('main .container');
+
+
+//topmenu mobile 버전
+gnbLi.forEach((el, idx) => {
+    el.addEventListener(("click"), (e) => {
+        e.preventDefault();
+        gnbLi[idx].classList.toggle('active');
+        gnbMenu2[idx].classList.toggle('active');
+    })
+})
 
 
 // 카테고리 버튼 클릭 시 leftmenu open, close
@@ -20,3 +33,4 @@ window.addEventListener("resize", () => {
         iframe.classList.remove('active');
     }
 })
+
